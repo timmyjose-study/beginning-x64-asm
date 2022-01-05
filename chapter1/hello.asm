fc8b2be936a39a3c736693a917325b2d76541a63
Timmy Jose <zoltan.jose@gmail.com>
2022-01-05 10:41:21 +0530

    Chapter 1 - Your First Program

--- chapter1/hello.asm
+++ chapter1/hello.asm
@@ -1,18 +1,19 @@
 ; hello.asm
 
 section .data
-  msg db 'Hello, world', 0x0, 0x0a
-  
+  msg db 'Hello, world', 0x0, 0xa 
+
 section .bss
 section .text
   global main
 
 main:
-  mov rax, 1 ; syswrite
+  mov rax, 1 ; sys_write
   mov rdi, 1 ; stdout
   mov rsi, msg
-  mov rdx, 12  ; length of msg
+  mov rdx, 14 ; with the \0 and \n
   syscall
-  mov rax, 60 ; exit
-  mov rdi, 0 ; exit code
+
+  mov rax, 60 ; sys_exit
+  mov rdi, 1 ; exit code
   syscall
--- chapter1/hello_with_len.asm
+++ chapter1/hello_with_len.asm
@@ -1,7 +1,7 @@
 ; hello_with_len.asm
 
 section .data
-  msg db 'Hello, world', 0x0, 0xa
+  msg db 'Hello, world',0x0,0xa
   msglen equ $-msg
 
 section .bss
@@ -9,20 +9,12 @@ section .text
   global main
 
 main:
-  ; function prologue
-  push rbp
-  mov rbp, rsp
-
   mov rax, 1 ; sys_write
   mov rdi, 1 ; stdout
   mov rsi, msg
   mov rdx, msglen
   syscall
 
-  ; function prologue
-  mov rsp, rbp
-  pop rbp
-
   mov rax, 60 ; sys_exit
-  mov rdi, 0 ; exit code
+  mov rdi, 0  ; exit code
   syscall
